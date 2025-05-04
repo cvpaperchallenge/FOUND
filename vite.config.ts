dfa76ae0631d1271107f83a1e5617ddd4f0dfceb
botany <yoshikikubotani.lab@gmail.com>
2025-05-05 00:22:45 +0900

    :bug: Fix the name conflict

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@ import path from "path"
 import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), reactRouter(), tailwindcss()],
+  plugins: [reactRouter(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
