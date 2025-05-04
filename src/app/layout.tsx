eff6257183951695e27ec92eafee28a8af12e5b7
botany <yoshikikubotani.lab@gmail.com>
2025-05-05 00:21:51 +0900

    :art: Add design tokens for background gradation color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,10 +4,12 @@ import { Header } from '@/components/header'
 
 export default function SharedUILayout() {
   return (
-    <div className="">
-      <Header />
-      <Outlet />
-      <Footer />
+    <div className="min-h-screen bg-gradient-to-br from-background from-20% via-background-gradation-1 via-50% to-background-gradation-2 to-90%">
+      <div className="bg-animated simple-grid flex min-h-screen w-full flex-col items-center overflow-x-hidden">
+        <Header />
+        <Outlet />
+        <Footer />
+      </div>
     </div>
   )
 }
