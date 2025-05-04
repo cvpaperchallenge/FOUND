d746a59f16448a46127b2317cab02499d0919787
botany <yoshikikubotani.lab@gmail.com>
2025-05-04 20:17:17 +0900

    :package: Introduce `react-router`

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 import path from "path"
+import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), reactRouter(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
